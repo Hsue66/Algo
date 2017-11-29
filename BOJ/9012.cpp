64464f863dc95e282e240d7d59761fbf56809ea3
hsue66 <dufmaakedl@naver.com>
2017-11-29 20:19:11 +0900

    practice boj

---
+++ BOJ/9012.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int testcase;
+	scanf("%d%*c",&testcase);
+	while(testcase--){
+		int top = 0;
+		int wrong = 0;
+		char piece = 0;
+		while(piece != 10 && scanf("%c",&piece)==1){
+			if(piece == '(')
+				top++;
+			else if(piece == ')'){
+				if(top == 0)
+					wrong = 1;
+				else
+					top--;
+			}
+		}
+		if(top != 0)
+			wrong = 1;
+
+		if(wrong)
+			printf("NO\n");
+		else
+			printf("YES\n");
+	}
+}
