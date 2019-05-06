7fb8c207ed55b46c63b1790277118f5611010f0a
Sumin Hong <dufmaakedl@gmail.com>
2019-05-06 16:44:55 +0900

    practice

--- BOJ/NOW/17143.cpp
+++ BOJ/NOW/17143.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<cstdio>
 #include<vector>
+#include<map>
 
 using namespace std;
 
@@ -13,36 +14,89 @@ struct shark{
   int r,c,s,d,z;
 };
 vector<shark> Sharks;
-int Dir[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};
+int Dir[4][2] = {{-1,0},{0,-1},{1,0},{0,1}};
 
 void moveSharks(){
-  vector<shark> NewSharks;
+  map<pair<int,int>,vector<int> > NewSharks;
   int lenS = Sharks.size();
   for(int i=0; i<lenS; i++){
     if(Sharks[i].z == NowZ)
       continue;
+
     int d = Sharks[i].d;
-    int tr = Sharks[i].r + (Sharks[i].s*Dir[d][0]);
-    int tc = Sharks[i].c + (Sharks[i].s*Dir[d][1]);
-    cout<<d<<" "<<Sharks[i].s<<endl;
-    cout<<Sharks[i].r<<" "<<Sharks[i].c<<"->"<<tr<<" "<<tc<<endl;
+    int size = R-1;
+    int loc = Sharks[i].r;
+    if(d%2 == 1){
+      size = C-1;
+      loc = Sharks[i].c;
+    }
+    int left = Sharks[i].s % (size*2);
+    while(left != 0){
+      int diff = loc;
+      if(d/2 == 1)
+        diff = size-loc;
+      if(left > diff){
+        loc = 0;
+        if(d/2 == 1)
+          loc = size;
+        left -= diff;
+        d = (d+2)%4;
+      }else{
+        if(d%2==0)
+          Sharks[i].r = loc + left * Dir[d][0];
+        else
+          Sharks[i].c = loc + left * Dir[d][1];
+        left = 0;
+      }
+    }
+    vector<int> temp(3);
+    temp[0] = Sharks[i].s;
+    temp[1] = d;
+    temp[2] = Sharks[i].z;
+    pair<int,int> pos = make_pair(Sharks[i].r,Sharks[i].c);
+
+    if(NewSharks.find(pos) != NewSharks.end()){
+      if(NewSharks[pos][2] < temp[2])
+        NewSharks[pos] = temp;
+    }else
+      NewSharks.insert(make_pair(pos,temp));
   }
+
+  vector<shark> nsharks;
+  for(auto it=NewSharks.begin(); it!=NewSharks.end(); ++it){
+    shark t;
+    t.r = (*it).first.first;
+    t.c = (*it).first.second;
+    t.s = (*it).second[0];
+    t.d = (*it).second[1];
+    t.z = (*it).second[2];
+
+    if(NowC == t.c){
+      if(NowR > t.r){
+        NowR = t.r;
+        NowZ = t.z;
+      }
+    }
+    nsharks.push_back(t);
+  }
+
+  swap(Sharks,nsharks);
 }
 
 void fishing(){
   while(NowC++ < C){
     //ㄱㅏ까운 상어 빠이
-    if(NowR != R){
+    // cout<<NowZ<<endl;
+    if(NowR != R)
       Sum += NowZ;
-
-    }
-    moveSharks();
     NowR = R;
-    NowZ = -1;
-    char a;
-    cin>>a;
+    moveSharks();
+    // cout<<"left: "<<Sharks.size()<<endl;
+    // char a;
+    // cin>>a;
   }
 }
+
 int main(){
   scanf("%d%d%d",&R,&C,&M );
   NowR = R;
@@ -56,6 +110,8 @@ int main(){
     if(t.d == 1)
       t.d = 2;
     else if(t.d == 2)
+      t.d = 3;
+    else if(t.d == 3)
       t.d = 1;
     Sharks.push_back(t);
     if(NowC == t.c){
--- BOJ/NOW/test.cpp
+++ BOJ/NOW/test.cpp
@@ -1,90 +1,24 @@
 #include<iostream>
 #include<cstdio>
+#include<map>
 #include<vector>
-#include<queue>
 
 using namespace std;
 
-vector<pair<pair<int,int>,vector<int> > > LiveT;
-int A[10][10];
-int Earth[10][10];
-
-int N,M,K;
-
 int main(){
-
-  vector<int> a;
-
-  vector<pair<pair<int,int>,vector<int> > >  L;
-  vector<int> t;
-  t.push_back(1);
-  L.push_back(make_pair(make_pair(1,2),t));
-
-  for(int i=0; i<L.size(); i++){
-    int len = L[i].second.size();
-    for(int j=0; j<len; j++)
-      cout<<L[i].second.at(j)<<" ";
+  for(int i=0; i<10; i++){
+    if(i==3)
+      continue;
+    cout<<i<<endl;
   }
-  cout<<endl;
+  map<pair<int,int>, vector<int> > Temp;
+  vector<int> t;
   t.push_back(3);
-  t.push_back(5);
+  t.push_back(2);
+  t.push_back(1);
+  Temp.insert(make_pair(make_pair(0,1),t));
 
-  for(int i=0; i<L.size(); i++)
-    L[i] = make_pair(L[i].first,t);
-
-    for(int i=0; i<L.size(); i++){
-      int len = L[i].second.size();
-      for(int j=0; j<len; j++)
-        cout<<L[i].second.at(j)<<" ";
-    }
-    cout<<"---------"<<endl;
-    for(int i=0; i<3; i++)
-      t.push_back(1);
-  for(int i=0; i<t.size(); i++)
-    cout<<t[i]<<" ";
-    cout<<endl;
-  // queue<pair<pair<int,int>, queue<int> > > T;
-  // queue<int> q;
-  // q.push(3);
-  // T.push(make_pair(make_pair(1,2),q));
-  //
-  // vector<int> a;
-  // a.push_back(3);
-  // a.push_back(2);
-  // a.push_back(1);
-  // a.push_back(5);
-  // for(int i=0; i<a.size(); i++)
-  //   cout<<a[i]<<endl;
-  //   sort(a.begin(),a.end());
-  // for(int i=0; i<a.size(); i++)
-  //   cout<<a[i]<<endl;
-
-//   vector<int> > > L;
-//   vector<int> t;
-//   t.push_back(1);
-//   L.push_back(make_pair(make_pair(1,2),t));
-//
-//   vector<pair<pair<int,int>, vector<int> > > M;
-//   for(int i=0; i<L.size(); i++){
-//     pair<int,int> pos = L[i].first;
-//     cout<<pos.first <<" "<<pos.second<<endl;
-//     vector<int> Ts = L[i].second;
-//     for(int t=0; t<Ts.size(); t++ ){
-//       cout<<Ts[t]<<endl;
-//       Ts[t]++;
-//     }
-//
-//     for(int t=0; t<Ts.size(); t++ ){
-//       cout<<Ts[t]<<" "<<L[i].second.at(t);
-//       //Ts[t]++;
-//     }
-//     M.push_back(make_pair(pos,Ts));
-//   }
-//   L = M;
-// cout<<endl;
-//   for(int i=0; i<L.size(); i++){
-//     vector<int> Ts = L[i].second;
-//     for(int t=0; t<Ts.size(); t++ ){
-//       cout<<Ts[t]<<endl;
-//     }
+  for(auto it=Temp.begin(); it!=Temp.end(); ++it){
+    cout<<(*it).second[0]<<(*it).first.second<<endl;
+  }
 }
